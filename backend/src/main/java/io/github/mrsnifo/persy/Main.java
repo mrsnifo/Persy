03724d23743819d9827aac3a1a38a92ef652c499
Sofien hichri <snifo@mail.com>
2025-12-26 00:48:32 +0100

    grizzly

--- backend/src/main/java/io/github/mrsnifo/persy/Main.java
+++ backend/src/main/java/io/github/mrsnifo/persy/Main.java
@@ -9,10 +9,7 @@ import java.net.URI;
 public class Main {
     public static void main(String[] args) {
         ResourceConfig rc = new ResourceConfig().packages("io.github.mrsnifo.persy.router");
-
-        int port = Config.SERVER_PORT;
-        URI uri = URI.create("http://localhost:" + port + "/api");
-
+        URI uri = URI.create("http://localhost:" + Config.SERVER_PORT + "/api");
         GrizzlyHttpServerFactory.createHttpServer(uri, rc);
         System.out.println("Server running at " + uri);
     }
